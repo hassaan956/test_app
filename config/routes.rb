452c1e2b65b4dc1d2da3cf3ae934a7ceaabd7a0b
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-05 22:40:25 +0500

    Initial commit and add root route

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  #give root route and controller application page path with mothod of hello which is to defined in application_controller.rb file.
+  root 'pages#home'
+end
