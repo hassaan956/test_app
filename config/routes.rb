ff211d091b1201dbd1c1a7fd8ecb61ec02629b55
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-06 21:27:47 +0500

    Add #about page, #home page, erb and ruby added to emmet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   #give root route and controller application page path with mothod of hello which is to defined in application_controller.rb file.
   root 'pages#home'
+  get 'about', to: 'pages#about'
 end
